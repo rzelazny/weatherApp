2202b7e25a8f2d58408832f616ecd460d0b6d561
Ryan Zelazny <RyanZelazny@gmail.com>
2020-11-14 20:31:21 -0500

    history works, but allows duplicates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //variables
     var APIKey = configVariables.apiKey;
 
     //function grabs the user input value
@@ -13,56 +14,112 @@ function searchWeather(searchValue){
         type:"GET",
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIKey + "&units=imperial",
         dataType: "json"
-        //success: function(){
         
     }).then(function(data){
-        console.log(data);
         //make sure there are no previous results
         $("#today").empty();
 
         //store city data in history
         saveCity(searchValue);
-
+        
         //creating a card to display the weather data
         var title = $("<h3>").addClass("card-title").text(data.name);
         var card = $("<div>").addClass("card");
+            //card.attr("id", "resultCard");
         var temp = $("<h4>").addClass("card-text").text("Temp: " + data.main.temp + "F");
-        var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed);
-        var humidity = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity);
+        var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + "MPH");
+        var humidity = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
         var cardBody = $("<div>").addClass("card-body");
-        
+            cardBody.attr("id", "resultCardBody");
 
+        //append current stats to card
         cardBody.append(title, temp, wind, humidity);
-        console.log(cardBody);
         card.append(cardBody);
         $("#today").append(card);
+
+        //get UV Data and append to card
+        getUVIndex(data.coord.lat, data.coord.lon);
+
+        //get forecast data
+        getForecast(searchValue);
+    })
+}
+// Add last ran cities to local storage and display history log
+function saveCity(city){
+
+    localStorage.setItem("savedCity", city);
+
+    var cityName = $("<h4>").addClass("card-title").text(city);
+    cityName.attr("class", "cityHistory")
+
+    // var card = $("<div>").addClass("card card-history");
+    // card.attr("id", city)
+    // var cardBody = $("<div>").addClass("card-body");
+
+    // cardBody.append(cityName);
+    // card.append(cardBody);
+    $("#history").append(cityName);
+
+    //function checks for user clicking city history
+    $(".cityHistory").on("click", function(){
+        searchWeather(this.innerHTML);
     })
 
-    
 }
-// .push cities to local storage
-function saveCity(city){
-    
-}
-
 
 //function to get the 5 day forecast
-function forecast(){
-    //use a for loop to 
+function getForecast(city){
+    $.ajax({
+        type:"GET",
+        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial",
+        dataType: "json"
+        
+    }).then(function(data){
+        console.log(data);
+        $("#forecast").empty();
+
+        for(i=0; i<data.list.length; i=i+8){ //weather updates are in 3 hour increments so we only need every 8th
+            //get the date from the time string and remove the time
+            var forecastDay = data.list[i].dt_txt;
+            forecastDay = forecastDay.split(' ')[0]; 
+            var title = $("<h3>").addClass("card-title").text(forecastDay);
+            
+            var temp = $("<h4>").addClass("card-text").text("Temp: " + data.list[i].main.temp + "F");
+            var humidity = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity);
+            var column = $("<div>").addClass("col-md-5ths");
+            var card = $("<div>").addClass("card");
+            var cardBody = $("<div>").addClass("card-body");
+    
+            //append current stats to card
+            cardBody.append(title, temp, humidity);
+            card.append(cardBody);
+            column.append(card)
+            $("#forecast").append(column);
+        }
+
+    })
 }
 
-//function to return UV Index
-
-
-
+//function to find UV Index and add it to results card
+function getUVIndex(lat, lon){
+    $.ajax({
+        type:"GET",
+        url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey,
+    }).then(function(data){
+        var uvIndex = $("<p>").addClass("card-text").text("UV Index: " + data.value);
+        
+        $("#resultCardBody").append(uvIndex);
+    })
+}
 
 //set inital variables and retrive saved data from local storage
 function init (){
-
+    var savedCity = localStorage.getItem("savedCity")
+    searchWeather(savedCity);
 }
 
+//
 
-
-
-
+//always load stored data
+init()
 })    
